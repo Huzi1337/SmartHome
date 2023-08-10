9564fc71a0a1d0c1a6130034d7bce07d3029e385
Huzi1337 <krzysztof.jagoda99@gmail.com>
2023-08-10 11:56:01 +0200

    Added mobile menu. Added animation to each page and selection buttons. Made the rooms screen responsive.

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -2,51 +2,64 @@ import dayjs from "dayjs";
 import "./SideMenu.scss";
 import { NavLink, useLocation } from "react-router-dom";
 import { Switch } from "@mantine/core";
+import { useState } from "react";
 
 const SECTIONS = ["home", "rooms", "users", "devices", "security"];
 
 const SideMenu = () => {
   const { pathname } = useLocation();
-  return (
-    <div className="sideMenu__container">
-      <div className="sideMenu__headerContainer">
-        <h1>Smart Home</h1>
-        <img
-          className="sideMenu__profilePic"
-          src="/photos/profilePic.png"
-        ></img>
-        <h4>{dayjs().format("DD MMM YYYY")}</h4>
-      </div>
+  const [showMenu, setShowMenu] = useState(false);
 
-      <nav className="sideMenu__navigation">
-        {SECTIONS.map((section) => {
-          const isActive = pathname === `/${section}` ? "active" : "";
-          return (
-            <NavLink
-              className={`sideMenu__navLink ${isActive}`}
-              key={section}
-              to={`/${section}`}
-            >
-              <div
-                key={section}
-                className={`sideMenu__navigation__icon ${section} ${isActive}`}
-              ></div>
-              <h3>{section}</h3>
-            </NavLink>
-          );
-        })}
-      </nav>
-      <div className="sideMenu__footer">
-        <div className="sideMenu__footerRow">
-          <h4>Dark Mode</h4>
-          <Switch aria-label="Dark Mode"></Switch>
+  const showMenuHandler = () => {
+    setShowMenu((prev) => !prev);
+  };
+  return (
+    <>
+      <div
+        onClick={showMenuHandler}
+        className={`sideMenu__blinder ${showMenu ? "show" : ""}`}
+      ></div>
+      <div className={`sideMenu__container ${showMenu ? "show" : ""}`}>
+        <button onClick={showMenuHandler} className="sideMenu__toggle"></button>
+        <div className="sideMenu__headerContainer">
+          <h1>Smart Home</h1>
+          <img
+            className="sideMenu__profilePic"
+            src="/photos/profilePic.png"
+          ></img>
+          <h4>{dayjs().format("DD MMM YYYY")}</h4>
         </div>
-        <div className="sideMenu__footerRow">
-          <div className="sideMenu__navigation__icon logout"></div>
-          <h3>Signout</h3>
+
+        <nav className="sideMenu__navigation">
+          {SECTIONS.map((section) => {
+            const isActive = pathname === `/${section}` ? "active" : "";
+            return (
+              <NavLink
+                className={`sideMenu__navLink ${isActive}`}
+                key={section}
+                to={`/${section}`}
+              >
+                <div
+                  key={section}
+                  className={`sideMenu__navigation__icon ${section} ${isActive}`}
+                ></div>
+                <h3>{section}</h3>
+              </NavLink>
+            );
+          })}
+        </nav>
+        <div className="sideMenu__footer">
+          <div className="sideMenu__footerRow">
+            <h4>Dark Mode</h4>
+            <Switch aria-label="Dark Mode"></Switch>
+          </div>
+          <div className="sideMenu__footerRow">
+            <div className="sideMenu__navigation__icon logout"></div>
+            <h3>Signout</h3>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
